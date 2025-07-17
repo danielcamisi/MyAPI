53e6819504383a2e9f347af6e3be6b65a8987e05
danielcamisi <danielcamillo2010@hotmail.com>
2025-07-17 15:47:39 -0300

    new route fix

--- routes/servRoutes.js
+++ routes/servRoutes.js
@@ -3,6 +3,6 @@ const servController = require("../controller/servController");
 
 const router = express.Router();
 
-router.post("https://my-api-five-delta.vercel.app", servController.create);
+router.post("https://my-api-five-delta.vercel.app/", servController.create);
 
 module.exports = router;
