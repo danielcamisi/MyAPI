b9989249149fc4d4a5cc877f97b56d027a5bd1f1
danielcamisi <danielcamillo2010@hotmail.com>
2025-07-23 18:37:48 -0300

    fix dot env

--- controller/servController.js
+++ controller/servController.js
@@ -6,6 +6,7 @@ exports.create = async (req, res, next) => {
     const newFeedback = new feed(req.body);
     await newFeedback.save();
     res.status(201).json(newFeedback);
+    console.log(newFeedback);
   } catch (error) {
     next(error);
   }
